a7e74c5de6166ed40048d1ee9d699af9f232db05
mr-fool <mr-fool@users.noreply.github.com>
2019-03-06 18:53:53 -0700

    lottery site

---
+++ precache-manifest.d1d4cd6b5c99b9589363b8481a9b27b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1176f420cb2b7af6e02a",
+    "url": "./static/css/main.a85484ba.chunk.css"
+  },
+  {
+    "revision": "1176f420cb2b7af6e02a",
+    "url": "./static/js/main.1176f420.chunk.js"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "b2cb0a460803286c74d3",
+    "url": "./static/js/2.b2cb0a46.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "39fa57aaef01fd7862ae57b14529d4b3",
+    "url": "./index.html"
+  }
+];
